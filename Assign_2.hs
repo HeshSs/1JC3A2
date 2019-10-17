c9940cd2ce6baad234d690c3ba75a8698665039e
HeshSs <hishmat.salehi2015@gmail.com>
2019-10-16 23:37:13 -0400

    Almost done the Extra Credit section of the assignment

--- Assign_2.hs
+++ Assign_2.hs
@@ -1,6 +1,6 @@
 {- Assignment 2
  - Name: Hishmat Salehi
- - Date: 5/10/2019
+ - Date: 16/10/2019
  -}
 module Assign_2 where
 
@@ -12,7 +12,7 @@ type GaussianInt = (Integer,Integer)
 {- -----------------------------------------------------------------
  - gaussReal
  - -----------------------------------------------------------------
- - Description: TODO add comments on gaussReal here
+ - Description: Returns the Real part of the Gaussian Integer
  -}
 gaussReal :: GaussianInt -> Integer
 gaussReal (x,y) = x
@@ -20,7 +20,7 @@ gaussReal (x,y) = x
 {- -----------------------------------------------------------------
  - gaussImag
  - -----------------------------------------------------------------
- - Description: TODO add comments on gaussImag here
+ - Description: Returns the Imaginary part of the Gaussian Integer
  -}
 gaussImag :: GaussianInt -> Integer
 gaussImag (x,y) = y
@@ -29,7 +29,7 @@ gaussImag (x,y) = y
 {- -----------------------------------------------------------------
  - gaussConj
  - -----------------------------------------------------------------
- - Description: TODO add comments on gaussConj here
+ - Description: Returns the conjugate of the inputted Gaussian Integer
  -}
 gaussConj :: GaussianInt -> GaussianInt
 gaussConj g = (gaussReal g, - (gaussImag g))
@@ -37,7 +37,7 @@ gaussConj g = (gaussReal g, - (gaussImag g))
 {- -----------------------------------------------------------------
  - gaussAdd
  - -----------------------------------------------------------------
- - Description: TODO add comments on gaussAdd here
+ - Description: Adds 2 Gaussian Integers and returns the result
  -}
 gaussAdd :: GaussianInt -> GaussianInt -> GaussianInt
 gaussAdd g0 g1 = ((gaussReal g0 + gaussReal g1), (gaussImag g0 + gaussImag g1))
@@ -45,7 +45,7 @@ gaussAdd g0 g1 = ((gaussReal g0 + gaussReal g1), (gaussImag g0 + gaussImag g1))
 {- -----------------------------------------------------------------
  - gaussMult
  - -----------------------------------------------------------------
- - Description: TODO add comments on gaussMult here
+ - Description: Multiplies 2 Gaussian Integers and returns the result
  -}
 gaussMult :: GaussianInt -> GaussianInt -> GaussianInt
 gaussMult g0 g1 = ((gaussReal g0 * gaussReal g1) - (gaussImag g0 * gaussImag g1),(gaussReal g0 * gaussImag g1) + (gaussImag g0 * gaussReal g1))
@@ -53,7 +53,7 @@ gaussMult g0 g1 = ((gaussReal g0 * gaussReal g1) - (gaussImag g0 * gaussImag g1)
 {- -----------------------------------------------------------------
  - gaussNorm
  - -----------------------------------------------------------------
- - Description: TODO add comments on gaussNorm here
+ - Description: The norm of a Gaussian integer is the multiplication of it with its conjugate
  -}
 gaussNorm :: GaussianInt -> Integer
 gaussNorm g = gaussReal (gaussMult g (gaussConj g))
@@ -61,7 +61,7 @@ gaussNorm g = gaussReal (gaussMult g (gaussConj g))
 {- -----------------------------------------------------------------
  - maxGaussNorm
  - -----------------------------------------------------------------
- - Description: TODO add comments on maxGaussNorm here
+ - Description: Uses recursion to go through a list of Gaussian Integers and returns the one with the highest norm
  -}
 maxGaussNorm :: [GaussianInt] -> GaussianInt
 maxGaussNorm gs
@@ -80,12 +80,132 @@ maxGaussNorm gs
  - -----------------------------------------------------------------
  -
  - -----------------------------------------------------------------
- - - Function:
- - - Test Case Number:
- - - Input:
- - - Expected Output:
- - - Acutal Output:
+ - - Function: gaussReal
+ - - Test Case Number: 1
+ - - Input: gaussReal (15,10)
+ - - Expected Output: 15
+ - - Actual Output: 15
  - -----------------------------------------------------------------
- - TODO: add test cases
+ - - Function: gaussReal
+ - - Test Case Number: 2
+ - - Input: gaussReal (1,5)
+ - - Expected Output: 1
+ - - Actual Output: 1
+ - -----------------------------------------------------------------
+ - - Function: gaussReal
+ - - Test Case Number: 3
+ - - Input: gaussReal (5,1)
+ - - Expected Output: 5
+ - - Actual Output: 5
+ - -----------------------------------------------------------------
+ - - Function: gaussImag
+ - - Test Case Number: 1
+ - - Input: gaussImag (15,10)
+ - - Expected Output: 10
+ - - Actual Output: 10
+ - -----------------------------------------------------------------
+ - - Function: gaussImag
+ - - Test Case Number: 2
+ - - Input: gaussImag (1,5)
+ - - Expected Output: 5
+ - - Actual Output: 5
+ - -----------------------------------------------------------------
+ - - Function: gaussImag
+ - - Test Case Number: 3
+ - - Input: gaussImag (5,1)
+ - - Expected Output: 1
+ - - Actual Output: 1
+ - -----------------------------------------------------------------
+ - - Function: gaussConj
+ - - Test Case Number: 1
+ - - Input: gaussConj (1,0)
+ - - Expected Output: (1,0)
+ - - Actual Output: (1,0)
+ - -----------------------------------------------------------------
+ - - Function: gaussConj
+ - - Test Case Number: 2
+ - - Input: gaussConj (5,10)
+ - - Expected Output: (5,-10)
+ - - Actual Output: (5,-10)
+ - -----------------------------------------------------------------
+ - - Function: gaussConj
+ - - Test Case Number: 3
+ - - Input: gaussConj (1,5)
+ - - Expected Output: (1,-5)
+ - - Actual Output: (1,-5)
+ - -----------------------------------------------------------------
+ - - Function: gaussAdd
+ - - Test Case Number: 1
+ - - Input: gaussAdd (2,0) (1,2)
+ - - Expected Output: (3,2)
+ - - Actual Output: (3,2)
+ - -----------------------------------------------------------------
+ - - Function: gaussAdd
+ - - Test Case Number: 2
+ - - Input: gaussAdd (-1,2) (1,2)
+ - - Expected Output: (0,4)
+ - - Actual Output: (0,4)
+ - -----------------------------------------------------------------
+ - - Function: gaussAdd
+ - - Test Case Number: 3
+ - - Input: gaussAdd (-1,2) (3,-4)
+ - - Expected Output: (2,-2)
+ - - Actual Output: (2,-2)
+ - -----------------------------------------------------------------
+ - - Function: gaussMult
+ - - Test Case Number: 1
+ - - Input: gaussMult (2,0) (1,2)
+ - - Expected Output: (2,4)
+ - - Actual Output: (2,4)
+ - -----------------------------------------------------------------
+ - - Function: gaussMult
+ - - Test Case Number: 2
+ - - Input: gaussMult (2,3) (1,4)
+ - - Expected Output: (-10,11)
+ - - Actual Output: (-10,11)
+ - -----------------------------------------------------------------
+ - - Function: gaussMult
+ - - Test Case Number: 3
+ - - Input: gaussMult (1,2) (3,4)
+ - - Expected Output: (-5,10)
+ - - Actual Output: (-5,10)
+ - -----------------------------------------------------------------
+ - - Function: gaussNorm
+ - - Test Case Number: 1
+ - - Input: gaussNorm (1,2)
+ - - Expected Output: 5
+ - - Actual Output: 5
+ - -----------------------------------------------------------------
+ - - Function: gaussNorm
+ - - Test Case Number: 2
+ - - Input: gaussNorm (5,10)
+ - - Expected Output: 125
+ - - Actual Output: 125
+ - -----------------------------------------------------------------
+ - - Function: gaussNorm
+ - - Test Case Number: 3
+ - - Input: gaussNorm (5,1)
+ - - Expected Output: 26
+ - - Actual Output: 26
+ - -----------------------------------------------------------------
+ - - Function: maxGaussNorm
+ - - Test Case Number: 1
+ - - Input: maxGaussNorm [(3,6),(1,3),(3,5),(7,1),(6,4)]
+ - - Expected Output: (6,4)
+ - - Actual Output: (6,4)
+ - -----------------------------------------------------------------
+ - - Function: maxGaussNorm
+ - - Test Case Number: 2
+ - - Input: maxGaussNorm [(1,2),(1,3),(6,5),(3,4),(3,3)]
+ - - Expected Output: (6,5)
+ - - Actual Output: (6,5)
+ - -----------------------------------------------------------------
+ - - Function: maxGaussNorm
+ - - Test Case Number: 3
+ - - Input: maxGaussNorm [(5,3),(1,3),(3,5),(3,4),(3,3)]
+ - - Expected Output: (5,3)
+ - - Actual Output:(5,3)
+ - -----------------------------------------------------------------
+ 
  -}
 
--- Assign_2_ExtraCredit.hs
+++ Assign_2_ExtraCredit.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE FlexibleContexts,FlexibleInstances,IncoherentInstances #-}
-{- Assignment 1 Extra Credit
+{- Assignment 2 Extra Credit
  - Name:Hishmat Salehi
- - Date: 6/10/2019
+ - Date: 16/10/2019
  -}
 module Assign_2_ExtraCredit where
 
@@ -21,13 +21,47 @@ class GaussianIntegral g where
   gaussAdd :: Integral a => g a -> g a -> g a
   gaussMult :: Integral a => g a -> g a -> g a
 
-{- TODO
- -   implement instances of GaussianIntegral
- -   implmenet instances of Eq, Ord
- -}
+instance GaussianIntegral GaussianInt where
+    gaussZero = (0 :@ 0)
+    gaussReal (x :@ y) = x
+    gaussImag (x :@ y) = y
+    gaussConj (x :@ y) = (x :@ (-y))
+    gaussAdd (a0 :@ b0) (a1 :@ b1) = ((a0 + a1) :@ (b0 + b1))
+    gaussMult (a0 :@ b0) (a1 :@ b1) = (((a0 * a1) - (b0 * b1)) :@ ((a0 * b1) + (b0 * a1)))
+
+instance GaussianIntegral Complex where
+    gaussZero = (0 :+ 0)
+    gaussReal (x :+ y) = x
+    gaussImag (x :+ y) = y
+    gaussConj (x :+ y) = (x :+ (-y))
+    gaussAdd (a0 :+ b0) (a1 :+ b1) = ((a0 + a1) :+ (b0 + b1))
+    gaussMult (a0 :+ b0) (a1 :+ b1) = (((a0 * a1) - (b0 * b1)) :+ ((a0 * b1) + (b0 * a1)))
+
+instance (Integral a) => Eq (GaussianInt a) where
+    (a0 :@ b0) == (a1 :@ b1) = (a0 == a1) && (b0 == b1)
+
+instance (Integral a) => Ord (GaussianInt a) where
+    (a0 :@ b0) < (a1 :@ b1) = (a0 < a1) && (b0 < b1)
+    (a0 :@ b0) > (a1 :@ b1) = (a0 > a1) && (b0 > b1)
+    (a0 :@ b0) >= (a1 :@ b1) = (a0 >= a1) && (b0 >= b1)
+    (a0 :@ b0) <= (a1 :@ b1) = (a0 <= a1) && (b0 <= b1)
+    max x y 
+        | x >= y    =  x
+        | otherwise =  y
+    min x y
+        | x <  y    =  x
+        | otherwise =  y
 
 gaussNorm :: (Integral a, GaussianIntegral g) => g a -> a
-gaussNorm g = error "TODO: implement gaussNorm"
+gaussNorm g = gaussReal (gaussMult g (gaussConj g))
 
-maxGaussNorm :: (Integral a, GaussianIntegral g) => [g a] -> g a
-maxGaussNorm gs = error "TODO: implement maxGaussNorm"
+maxGaussNorm :: (Integral a, GaussianIntegral g, Eq (g a)) => [g a] -> g a
+maxGaussNorm gs 
+           | gs == [] = gaussZero
+           | (length gs) == 1 = head gs
+           | otherwise = if gaussNorm x0 < gaussNorm x1
+                            then maxGaussNorm ([x1] ++ xs)
+                            else maxGaussNorm ([x0] ++ xs)
+                                    where x0 = head gs
+                                          x1 = head (tail gs)
+                                          xs = tail (tail gs)
